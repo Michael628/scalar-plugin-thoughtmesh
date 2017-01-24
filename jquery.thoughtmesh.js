6ef944e316badb4c219b8e2cd195dcc786e75230
Craig Dietrich <craigdietrich@gmail.com>
2017-01-23 21:23:08 -0800

    Style updates

--- jquery.thoughtmesh.js
+++ jquery.thoughtmesh.js
@@ -1,11 +1,12 @@
+/**
+ * @required bootbox
+ */
+
 (function( $ ) {
 	
-	var defaults = {
-        'results': [
-        {
-            'text':"Articles",
-            'keys':["Related Keywords"]
-        },
+	var defaults = {};
+	
+	var results = [  // Temp
         {
             'author':"Palin",
             'text':"Why I endorsed Donald Trump",
@@ -25,147 +26,109 @@
             'author':"Gonzalez",
             'text':"An analysis of community interfaces...",
             'keys':['collaboration']
-        }],
-        'style':{
-            'color':'#26d19d',
-            'bg': ''
-        }
-	};  	
+        }];
+	
+	var related = {
+            'internal': [{
+                'author':'Michael Lynch',
+                'title':'Twin Paradox',
+                'lexia':'It was the best of times',
+                'excerpt':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },{
+                'author':'George R.R. Martin',
+                'title':'When?',
+                'lexia':'Soon',
+                'excerpt':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }],
+            'external': [{
+                'author':'Richard Feynman',
+                'title':'You Must Be Joking...',
+                'lexia':'Path Integrals',
+                'excerpt':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },{
+                'author':'Bertrand Russell',
+                'title':'Logicomix',
+                'lexia':'Let\'s play a game',
+                'excerpt':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },{
+                'author':'Paul Dirac',
+                'title':'Relativistic Quantum Mechanics',
+                'lexia':'We got this',
+                'excerpt':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }]
+        };
 	
     $.fn.thoughtmesh = function(options) {
         var self = this;
         var $this = $(this);
         var opts = $.extend( {}, defaults, options );
-        var base = opts.style.color;
+        $.getScript($('link#approot').attr('href')+'plugins/thoughtmesh/lib/bootbox.min.js', function(data,textStatus,jqxhr) {});
+        
+        var openModal = function() {
+                var self = this;
+                var $this = $(this);
+                var tag = $this.data('tm-tag');
+                var results = getRelated(tag);
 
-        var adjustColor = function  (col, amt) {
-          
-            var usePound = false;
-          
-            if (col[0] == "#") {
-                col = col.slice(1);
-                usePound = true;
-            }
-         
-            var num = parseInt(col,16);
-         
-            var r = (num >> 16) + amt;
-         
-            if (r > 255) r = 255;
-            else if  (r < 0) r = 0;
-         
-            var b = ((num >> 8) & 0x00FF) + amt;
-         
-            if (b > 255) b = 255;
-            else if  (b < 0) b = 0;
-         
-            var g = (num & 0x0000FF) + amt;
-         
-            if (g > 255) g = 255;
-            else if (g < 0) g = 0;
-         
-            return (usePound?"#":"") + ('0'+r.toString(16)).slice(-2) + ('0'+b.toString(16)).slice(-2) + ('0'+g.toString(16)).slice(-2);
-        }
+                bootbox.dialog({
+                	size: 'large',
+                    message: '<div id="bootbox-thoughtmesh-content" class="heading_font"></div>',
+                    title: 'ThoughtMesh pages related to <b>'+tag+'</b>',
+                    className: 'thoughtmesh_bootbox',
+                    animate: ( (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) ? false : true )// Panel is unclickable if true for iOS
+                });
+                $('.thoughtmesh_bootbox .modal-body').height( parseInt($(window).height()) * 0.83 );
+                $('.bootbox-close-button').empty();
+                $('.bootbox').find( '.modal-title' ).addClass( 'heading_font' );
+                var $box = $('<div />').addClass('container-fluid').appendTo($('#bootbox-thoughtmesh-content'));
+                $('<div>Excerpts Here</div>').appendTo($box).addClass('h5 heading_font col-md-12').wrap($('<div />').addClass('row'));
+                var $internal = $('<div />').addClass('row').appendTo($box);
+                for(var i in results['internal']) {
+                    var entry = results['internal'][i];
+                    $('<div></div>').appendTo($internal).html(entry.author+',&nbsp;"'+entry.title+'"').addClass('col-md-12 tm-header');
+                    $('<div></div>').appendTo($internal).html(entry.lexia).addClass('col-md-11 col-md-offset-1 tm-text');
+                    $('<div></div>').appendTo($internal).html(entry.excerpt).addClass('col-md-11 col-md-offset-1 tm-excerpt');
+                };
+                $('<div>Excerpts Out</div>').appendTo($box).addClass('h5 heading_font col-md-12').wrap($('<div />').addClass('row'));
+                var $external = $('<div />').addClass('row').appendTo($box);
+                for(var i in results['external']) {
+                    var entry = results['external'][i];
+                    $('<div></div>').appendTo($external).html(entry.author+',&nbsp;"'+entry.title+'"').addClass('col-md-12 tm-header');
+                    $('<div></div>').appendTo($external).html(entry.lexia).addClass('col-md-11 col-md-offset-1 tm-text');
+                    $('<div></div>').appendTo($external).html(entry.excerpt).addClass('col-md-11 col-md-offset-1 tm-excerpt');
+                };
+        };
 
         var getRelated = function(tag) {
-            return {
-                'internal': [{
-                    'author':'Michael Lynch',
-                    'title':'Twin Paradox',
-                    'lexia':'It was the best of times',
-                    'excerpt':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-                },{
-                    'author':'George R.R. Martin',
-                    'title':'When?',
-                    'lexia':'Soon',
-                    'excerpt':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-                }],
-                'external': [{
-                    'author':'Richard Feynman',
-                    'title':'You Must Be Joking...',
-                    'lexia':'Path Integrals',
-                    'excerpt':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-                },{
-                    'author':'Bertrand Russell',
-                    'title':'Logicomix',
-                    'lexia':'Let\'s play a game',
-                    'excerpt':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-                },{
-                    'author':'Paul Dirac',
-                    'title':'Relativistic Quantum Mechanics',
-                    'lexia':'We got this',
-                    'excerpt':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-                }]
-            }
+            return related;
         };
 
-        var openModal = function() {
-            var self = this;
-            var $this = $(this);
-            var tag = $(this).attr('tm-tag');
-            var results = getRelated(tag);
-
-            bootbox.dialog({
-                message: '<div id="bootbox-thoughtmesh-content" class="heading_font"></div>',
-                title: 'Pages related to "'+tag+'"',
-                className: 'thoughtmesh_bootbox',
-                animate: ( (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) ? false : true )// Panel is unclickable if true for iOS
-            });
-            $('.bootbox-close-button').empty();
-
-            $('.bootbox').find( '.modal-title' ).addClass( 'heading_font' );
-
-            var $box = $('<div />').addClass('container-fluid').appendTo($('#bootbox-thoughtmesh-content'));
-            $('<div>Scalar Pages</div>').appendTo($box).addClass('h5 heading_font col-md-12').wrap($('<div />').addClass('row'));
-            var $internal = $('<div />').addClass('row tm-scroll').appendTo($box);
-            for(var i in results['internal']) {
-                var entry = results['internal'][i];
-                $('<div></div>').appendTo($internal).html(entry.author+',&nbsp;"'+entry.title+'"').addClass('col-md-12 tm-header');
-                $('<div></div>').appendTo($internal).html(entry.lexia).addClass('col-md-11 col-md-offset-1 tm-text');
-                $('<div></div>').appendTo($internal).html(entry.excerpt).addClass('col-md-11 col-md-offset-1 tm-excerpt');
+        var $wrapper = $('<div class="tm_footer container-fluid"></div>').appendTo($this);
+        var $header = $("<div class='row'><div class='col-md-2 col-sm-2 col-xs-4'></div><div class='col-md-5 col-xs-8 col-sm-6'><div class=\"tm-header\">Articles</div></div><div class='col-md-4 col-sm-4 hidden-xs'><div class=\"tm-header\">Related keywords</div></div></div>").appendTo($wrapper);
+        for (var i in results) {
+            var $row = $("<div class='row'><div class='tm-tag col-md-2 col-sm-2 col-xs-4'></div><div class='tm-article col-md-5 col-xs-8 col-sm-6'></div><div class='tm-key col-md-4 col-sm-4 hidden-xs'></div></div>").appendTo($wrapper);
+            var $article = $row.children('.tm-article:first');
+            // Author
+            if ('undefined' != typeof(results[i].author)) {
+                $("<span class='tm-author'></span>").html(results[i].author+',&nbsp;').appendTo($article);
             }
-            $('<div />').html('<hr />').addClass('row').wrapInner($('<div />').addClass('col-md-12')).appendTo($box);
-
-            $('<div>Thoughtmesh Lexias</div>').appendTo($box).addClass('h5 heading_font col-md-12').wrap($('<div />').addClass('row'));
-            var $external = $('<div />').addClass('row tm-scroll').appendTo($box);
-            for(var i in results['external']) {
-                var entry = results['external'][i];
-                $('<div></div>').appendTo($external).html(entry.author+',&nbsp;"'+entry.title+'"').addClass('col-md-12 tm-header');
-                $('<div></div>').appendTo($external).html(entry.lexia).addClass('col-md-11 col-md-offset-1 tm-text');
-                $('<div></div>').appendTo($external).html(entry.excerpt).addClass('col-md-11 col-md-offset-1 tm-excerpt');
-            }
-        };
-
-        $this.append('<div id="tm_footer" class="col-md-9 col-sm-12 col-xs-12"></div>');
-        for(var i in opts.results) {
-            var $row = $("<div class='row'><div class='tm-tag col-md-2 col-sm-2 col-xs-4'></div><div class='tm-article col-md-5 col-xs-8 col-sm-6'></div><div class='tm-key col-md-4 col-sm-4 hidden-xs'></div></div>");
-            var $article = $row.children('.tm-article');
-
-            if(typeof(opts.results[i].author) != 'undefined'){
-                $("<span class='tm-author'></span>").html(opts.results[i].author+',&nbsp;').appendTo($article);
-            }
-            $("<span class='tm-text'></span>").html(opts.results[i].text).appendTo($article);
-
-            $row.children('.tm-tag').html(('<span class="glyphicon glyphicon-tag"></span>').repeat(opts.results[i].keys.length));
-
+            // Text
+            $("<span class='tm-text'></span>").html(results[i].text).appendTo($article);
+            // Tags
+            $row.children('.tm-tag').html(('<span class="glyphicon glyphicon-tag"></span>').repeat(results[i].keys.length));
+            // Keywords
             var keyhtml = '';
             $keys = $row.children('.tm-key');
-
-            for(var j in opts.results[i].keys) {
-                if(j!=0)
-                    $keys.append(',&nbsp;');
+            for (var j = 0; j < results[i].keys.length; j++) {
+                if (0 != j) $keys.append(',&nbsp;');
                 var $key = $('<span></span>');
                 $key.addClass('tm-link');
-                $key.html(opts.results[i].keys[j]);
-                $key.attr('tm-tag',opts.results[i].keys[j]);
+                $key.html(results[i].keys[j]);
+                $key.data('tm-tag',results[i].keys[j]);
                 $key.appendTo($keys);
-            }
-            $("#tm_footer").append($row);
+            };
         }
-        $('.tm-link:first').removeClass('tm-link');
-        $('.tm-key:first, .tm-text:first').addClass('tm-header');
         $('.tm-link').click(openModal);
         $('.tm-tag').wrapInner('<div style="float:right"></div>');
-        $('.tm-tag:first').empty();
     }
 }( jQuery ));
